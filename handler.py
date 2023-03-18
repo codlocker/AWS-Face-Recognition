2c2249e29eb54b8cfd4088b828bf092e6320a660
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-03-17 19:43:36 -0700

    Inital changes for facial recognition handler

--- handler.py
+++ handler.py
@@ -1,9 +1,15 @@
-from boto3 import client as boto3_client
+import boto3
 import face_recognition
+import os
 import pickle
+from termcolor import cprint
+import colorama
 
-input_bucket = "546proj2"
+colorama.init()
+input_bucket = 'inputbucket-cse546'
 output_bucket = "546proj2output"
+encoding_filename = "encoding"
+frames_path = os.path.join(os.getcwd(), "Frames")
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
@@ -12,5 +18,55 @@ def open_encoding(filename):
 	file.close()
 	return data
 
-def face_recognition_handler(event, context):	
-	print("Hello")
+def face_recognition_handler(file_name):	
+	local_file_path = download_file_s3(file_name)
+	encoded_data = open_encoding(encoding_filename)
+
+	cprint(f"Frames folder: {frames_path}", "magenta")
+	cmd = f'''ffmpeg -i "{str(local_file_path)}" -r 1 "{str(frames_path)}\image-%3d.jpeg" -hide_banner -loglevel error'''
+
+	cprint(f"Executing: {cmd}", "blue")
+
+	# 1. Split video into frames
+	os.system(cmd)
+
+	# 2. Face-recog each frame file
+
+	fileList = os.listdir(frames_path)
+
+	for file in fileList:
+		unknown_image = face_recognition.load_image_file(f"{frames_path}/{file}")
+		unknown_image_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+		cprint(f"Comparing the encoding for file name : {file}", "yellow")
+
+		for name, encoded_value in zip(encoded_data["name"], encoded_data["encoding"]):
+			print(name)
+
+			results = face_recognition.compare_faces(unknown_image_face_encoding, [encoded_value])
+
+			print(results)
+
+		 
+
+
+
+# Define function to download file from S3 bucket
+def download_file_s3(file_name):
+	s3 = boto3.resource('s3')
+	bucket = s3.Bucket(input_bucket)
+	local_file_path = os.path.join(os.getcwd(), file_name)
+
+	cprint(f"Local video path : {local_file_path}", "magenta")
+	if not os.path.exists(local_file_path):
+		cprint("Downloading file...", "blue")
+		bucket.download_file(file_name, local_file_path)
+	return local_file_path
+
+
+if not os.path.exists(frames_path):
+	os.makedirs(frames_path)
+
+face_recognition_handler('test_0.mp4')
+
+
