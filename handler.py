c0c463f7b1a1e467ffc21a483455c574d21a5488
Ipsit Sahoo <12949012+codlocker@users.noreply.github.com>
2023-03-20 12:44:08 -0700

    User/isahoo1/plan Final push to this jke (#11)
    * update
    
    * Add ls to docker
    
    * Add encoding.dat to docker
    
    * Add something
    
    * Add something
    
    * Adding delete for csv and video files
    
    ---------
    
    Co-authored-by: Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>

--- handler.py
+++ handler.py
@@ -14,7 +14,11 @@ encoding_filename = "encoding.dat"
 
 # Validate whether you are in a docker or machine.
 environ_key = os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False)
+
+# Initialize variables data_folder for storing files, upload_status to output bucket, CSV File name if created.
 data_folder = None
+upload_status = False
+csvFileName = None
 
 print("Environ Key:", environ_key)
 
@@ -35,74 +39,77 @@ def open_encoding(filename):
 	return data
 
 def face_recognition_handler(event, context):
-	print(event)
+	try:
+		print(event)
 
-	if type(event) == dict:
-		file_name = event['Records'][0]['s3']['object']['key']
-	else:
-		file_name = event
+		if type(event) == dict:
+			file_name = event['Records'][0]['s3']['object']['key']
+		else:
+			file_name = event
 
-	# 0. Build the baseline
-	frames_path = os.path.join(data_folder, "Frames") + "/"
-	if not os.path.exists(frames_path):
-		os.makedirs(frames_path)
+		# 0. Build the baseline
+		frames_path = os.path.join(data_folder, "Frames") + "/"
+		if not os.path.exists(frames_path):
+			os.makedirs(frames_path)
 
 
-	local_file_path = download_file_s3(file_name)
-	encoded_data = open_encoding(encoding_filename)
+		local_file_path = download_file_s3(file_name)
+		encoded_data = open_encoding(encoding_filename)
 
-	cprint(f"Frames folder: {frames_path}", "magenta")
-	cmd = f'''ffmpeg -i "{str(local_file_path)}" -r 1 "{str(frames_path)}image-%3d.jpeg" -hide_banner -loglevel error'''
+		cprint(f"Frames folder: {frames_path}", "magenta")
+		cmd = f'''ffmpeg -i "{str(local_file_path)}" -r 1 "{str(frames_path)}image-%3d.jpeg" -hide_banner -loglevel error'''
 
-	cprint(f"Executing: {cmd}", "blue")
+		cprint(f"Executing: {cmd}", "blue")
 
-	# 1. Split video into frames
-	os.system(cmd)
+		# 1. Split video into frames
+		os.system(cmd)
 
-	# 2. Face-recog each frame file to get the first face
+		# 2. Face-recog each frame file to get the first face
 
-	fileList = os.listdir(frames_path)
+		fileList = os.listdir(frames_path)
 
-	faceName = ""
+		faceName = ""
 
-	for file in fileList:
-		print(f"Located image file : {frames_path}{file}")
-		unknown_image = face_recognition.load_image_file(f"{frames_path}{file}")
-		unknown_image_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+		for file in fileList:
+			print(f"Located image file : {frames_path}{file}")
+			unknown_image = face_recognition.load_image_file(f"{frames_path}{file}")
+			unknown_image_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
-		cprint(f"Comparing the encoding for file name : {file}", "yellow")
+			cprint(f"Comparing the encoding for file name : {file}", "yellow")
 
-		for name, encoded_value in zip(encoded_data["name"], encoded_data["encoding"]):
-			results = face_recognition.compare_faces(unknown_image_face_encoding, [encoded_value])
-			# print(name, bool(results[0]))
-			if results[0] == True:
+			for name, encoded_value in zip(encoded_data["name"], encoded_data["encoding"]):
+				results = face_recognition.compare_faces(unknown_image_face_encoding, [encoded_value])
+				# print(name, bool(results[0]))
+				if results[0] == True:
 
-				faceName = name
+					faceName = name
+					break
+			if faceName:
 				break
-		if faceName:
-			break
 
-	# 3. Search facename in DynamoDB
-	
-	result = search_in_dynamodb(facename=faceName)
+		# 3. Search facename in DynamoDB
+		
+		result = search_in_dynamodb(facename=faceName)
 
-	# 4. Upload CSV file to S3 bucket.
-	upload_status = False
-	csvFileName = None
-	if result:
-		csvFileName = file_name.split('.')[0] + ".csv"
-		csv_file_path = f"{data_folder}{csvFileName}"
-		print(f"CSV file at {csv_file_path}")
+		# 4. Upload CSV file to S3 bucket.
+		if result:
+			csvFileName = file_name.split('.')[0] + ".csv"
+			csv_file_path = f"{data_folder}{csvFileName}"
+			print(f"CSV file at {csv_file_path}")
 
-		with open(f'{csv_file_path}', 'w') as f:
-			f.writelines(result)
+			with open(f'{csv_file_path}', 'w') as f:
+				f.writelines(result)
 
-		upload_csv_to_bucket(
-			csv_file=csvFileName)
-		upload_status = True
+			upload_csv_to_bucket(
+				csv_file=csvFileName)
+			upload_status = True
 
-	# 5. Clean Up if Step 4 succeeds
-	if upload_status:
+		return result
+
+	except Exception as e:
+		print(e)
+	finally:
+		# 5. Clean Up if Step 4 succeeds
 		# Remove all files in Frames folder
 		print("Clean up script starts...")
 		files = glob.glob(frames_path + "/*")
@@ -110,7 +117,14 @@ def face_recognition_handler(event, context):
 			cprint(f"Deleting file {f}", "red")
 			os.remove(f)
 
-	return result
+		# Delete video file
+		if file_name:
+			cprint(f"Deleting Video file {file_name}", "red")
+			os.remove(os.path.join(data_folder, file_name))
+		# Delete CSV file
+		if csvFileName:
+			cprint(f"Deleting CSV file {csvFileName}", "red")
+			os.remove(os.path.join(data_folder, csvFileName))
 
 
 # Define function to download file from S3 bucket
@@ -159,10 +173,7 @@ def upload_csv_to_bucket(csv_file):
 		exit(-1)
 
 
-# This will change to actual format
-# face_recognition_handler(event, context)
-# print(face_recognition_handler('test_0.mp4', None))
 
-# print(search_in_dynamodb("president_biden"))
+# print(face_recognition_handler('test_7.mp4', None))
 
 
